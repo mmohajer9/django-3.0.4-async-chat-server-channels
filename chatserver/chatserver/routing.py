dbe8f98b05d4638042b925c6a87cf00b60cd785a
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2020-03-07 13:44:29 +0330

    initial files also added more of these files

--- chatserver/chatserver/routing.py
+++ chatserver/chatserver/routing.py
@@ -1,5 +1,27 @@
-from channels.routing import ProtocolTypeRouter
+from django.urls import path , re_path
+from django.conf.urls import url
+from channels.routing import ProtocolTypeRouter , URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator , OriginValidator
 
+from chat.consumers import ChatConsumer
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
+
+    #? wrap around the websocket : making sure that what ever host or domain name is doing request ,
+    #? it will match the ALLOWED_HOSTS in settings.py -- scope of security - allowed hosts
+    "websocket" : AllowedHostsOriginValidator(
+
+        #? scope of users
+        AuthMiddlewareStack(
+            
+            #? similar to urls.py but async version !
+            URLRouter(
+                [
+
+                ]
+            )
+        )
+
+    )
 })
--- chatserver/chatserver/settings.py
+++ chatserver/chatserver/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'kad36ins3bj-jmxogi5$hmbahu_pcx3d(p1ky#zp-@z^g%ch*3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
